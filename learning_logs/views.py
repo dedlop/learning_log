08e57a10068e31459d49a022cca0b330067d1a4a
dedlop <104914772+dedlop@users.noreply.github.com>
2024-06-23 20:16:01 -0300

    Criando formulários

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     """Página principal do Learnig_Log"""
@@ -10,3 +13,25 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = TopicForm()
+    else:
+        # Dados de POST submetidos; processo os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
